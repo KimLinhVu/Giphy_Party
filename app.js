06a560837117da9eca9c736fe72168b78156680b
Kim-Linh Vu <kimlinh@fb.com>
2022-06-16 10:00:02 -0700

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.search')
+const input = document.querySelector('.input')
+const results_div = document.querySelector('.results')
+const api_key = "55hhPS3sSZDvYFrWRpCHeS8fTutClh8J"
 
+
+function displayResults(gifs){
+    gifs.forEach((gif) => {
+        results_div.innerHTML += `
+        <img src="${gif.images.original.url}" >
+        `
+    })
+}
+
+async function fetchGifs(search){
+    const res = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${search}`)
+    const data = await res.json()
+    console.log(data.data)
+    displayResults(data.data)
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    let search = input.value
+    console.log(search)
+    fetchGifs(search)
+})
